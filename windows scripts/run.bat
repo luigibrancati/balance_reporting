286c0d15fde245609536f46a0b3b46f959a1c0a8
Luigi Brancati <luigi.brancati93@gmail.com>
2021-10-09 22:43:20 +0200

    Updated windows script

--- windows scripts/run.bat
+++ windows scripts/run.bat
@@ -5,4 +5,4 @@ if errorlevel 1 (
    call conda create -n balance python==3.9 -y
 )
 call conda activate balance && pip install -r ../Dash/requirements.txt
-cd ..\Dash && python report_account.py
+cd ..\Dash && python report_account.py DataCristina
