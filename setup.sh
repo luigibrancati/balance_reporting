b24c0257e33554789aa8aaad39db7aedb936984c
luigi.brancati <luigi.brancati93@gmail.com>
2023-04-01 19:50:28 +0200

    Small fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+echo "\
 [server]\n\
-headless = false\n\
-address = 0.0.0.0\n\
+headless = true\n\
+address = '0.0.0.0'\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
