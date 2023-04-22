cceb8afb4811f56af8f33cf4c8a3add222d748fa
luigi.brancati <luigi.brancati93@gmail.com>
2023-04-22 18:44:16 +0200

    Added download

--- src/file_manager.py
+++ src/file_manager.py
@@ -2,16 +2,26 @@ from config import DATA_FOLDER
 import streamlit as st
 import os
 from glob import glob
+import polars as pl
+from pathlib import Path
+from typing import Generator
 
-def list_files():
-    yield from glob(f'{DATA_FOLDER}/{st.session_state.username}/*.csv')
+def list_files() -> Generator[Path, None, None]:
+    return (Path(p) for p in glob(f'{DATA_FOLDER}/{st.session_state.username}/*.csv'))
 
-def delete_file(file):
+def delete_file(file:Path) -> None:
     os.remove(file)
 
-def file_lister():
+@st.cache_data
+def convert_df(filepath:Path) -> bytes:
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    df = pl.read_csv(filepath, has_header=True, separator=';', try_parse_dates=True)
+    return df.write_csv().encode('utf-8')
+
+def file_lister() -> None:
     with st.expander("Saved files"):
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
         for file in list_files():
-            col1.write(file.split('/')[-1])
-            col2.button("Delete", on_click=lambda: delete_file(file), key=file)
+            col1.write(file.name)
+            col2.download_button("Download", data=convert_df(file), file_name=file.name)
+            col3.button("Delete", on_click=lambda: delete_file(file), key=file)
